37edd2b8d7781f9df2c4110159bcc3d038bb58dc
VectorLim <yeapzing@gmail.com>
2022-09-13 14:19:25 +0800

    minor fixes and touch-ups

--- src/pyencoder/ArithmeticCoding/AdaptiveArithmeticCoding/codebook.py
+++ src/pyencoder/ArithmeticCoding/AdaptiveArithmeticCoding/codebook.py
@@ -5,11 +5,60 @@ from typing import OrderedDict, Tuple
 from pyencoder import Settings
 
 
+def getsum(BITTree, i):
+    s = 0  # initialize result
+
+    # index in BITree[] is 1 more than the index in arr[]
+    i = i + 1
+
+    # Traverse ancestors of BITree[index]
+    while i > 0:
+
+        # Add current element of BITree to sum
+        s += BITTree[i]
+
+        # Move index to parent node in getSum View
+        i -= i & (-i)
+    return s
+
+
+# Updates a node in Binary Index Tree (BITree) at given index
+# in BITree. The given value 'val' is added to BITree[i] and
+# all of its ancestors in tree.
+def updatebit(BITTree, n, i, v):
+
+    # index in BITree[] is 1 more than the index in arr[]
+    i += 1
+
+    # Traverse all ancestors and add 'val'
+    while i <= n:
+
+        # Add 'val' to current node of BI Tree
+        BITTree[i] += v
+
+        # Update index to that of parent in update View
+        i += i & (-i)
+
+
+# Constructs and returns a Binary Indexed Tree for given
+# array of size n.
+def construct(arr, n):
+
+    # Create and initialize BITree[] as 0
+    BITTree = [0] * (n + 1)
+
+    # Store the actual values in BITree[] using update()
+    for i in range(n):
+        updatebit(BITTree, n, i, arr[i])
+
+    # Uncomment below lines to see contents of BITree[]
+    # for i in range(1,n+1):
+    #     print BITTree[i],
+    return BITTree
+
+
 class AdaptiveArithmeticCodebook:
     def __init__(self):
-        self.reset()
-
-    def reset(self) -> None:
         self.symbol_catalogue = OrderedDict({k: 1 for k in Settings.SYMBOLS})
         self.symbol_counts = sum(self.symbol_catalogue.values())
         self.symbol_probability_bounds = list(itertools.accumulate(self.symbol_catalogue.values(), initial=0))
@@ -28,7 +77,10 @@ class AdaptiveArithmeticCodebook:
     def catalogue_symbol(self, symbol: str) -> Tuple[int, int]:
         sym_probs = self.symbol_probability_bounds
 
-        index = Settings.SYMBOLS.index(symbol)
+        try:
+            index = Settings.SYMBOLS.index(symbol)
+        except ValueError as err:
+            raise ValueError("unknown symbol detected: {0}".format(symbol)) from err
         probability = (sym_probs[index], sym_probs[index + 1])
 
         self._update(symbol, index)
